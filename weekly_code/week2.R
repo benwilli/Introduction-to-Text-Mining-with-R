a147a7409baf29a694a16f57e83cfc2d254fcfd5
benwilli <benjamin@smu.edu>
2017-08-05 17:06:38 -0600

    Update week2.R

--- weekly_code/week2.R
+++ weekly_code/week2.R
@@ -6,7 +6,22 @@ library(tidyverse)
 #what is the structure of our example data
 str(mpg)
 
-#base R
+#look at the first few rows of the dataframe
+head(mpg)
+
+#look at the last few rows of the dataframe
+tail(mpg)
+
+#look at the variable "manufacturer"
+mpg$manufacturer
+
+#look at the variable "hwy"
+mpg$hwy
+
+#summarize the variable "hwy"
+summary(mpg$hwy)
+
+#base R plot
 plot(displ~hwy,data=mpg)
 
 #using functin from package ggplot2, which was loaded into R by package tidyverse
